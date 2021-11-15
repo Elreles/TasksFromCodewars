bc15104a6c0a2d1dc79ae6853cfc426cbd9ebb20
Вячеслав Юрочка <nodokohhik@gmail.com>
2021-11-15 22:55:07 +0300

    added task Fibonacci

---
+++ TasksFromCodewars/Tasks/Fibonacci.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//not from codewars =)
+namespace TasksFromCodewars.Tasks
+{
+    internal class Fibonacci
+    {
+
+        public static void FibonacciMethod()
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(FibonacciFunc(n));
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+        public static int FibonacciFunc(int n)
+        {
+            if (n < 3)
+            {
+                return 1;
+            }
+            else
+            {
+                return (FibonacciFunc(n - 1) + FibonacciFunc(n - 2));
+            }
+        }
+    }
+}
